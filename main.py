d6840f9f83912417a72d30125c9635adaea50f4e
shashwattyagi13 <shashwattyagi988.com>
2021-02-15 01:04:44 +0530

    renaming

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # All media file is available for download as a zip file
-import tkinter 
-import cv2 # pip install opencv-python
-import PIL.Image, PIL.ImageTk # pip install pillow
+import tkinter                            #Tkinter is our inbuilt package no need to install it..
+                                          #It is a standard Python interface to the Tk GUI toolkit shipped with Python. Python with tkinter is the fastest and easiest way to create the GUI applications
+import cv2 # pip install opencv-python                    //it is module    //OpenCV is a cross-platform library using which we can develop real-time computer vision applications. It mainly focuses on image processing, video capture and analysis including features like face detection and object detection.
+                                                     # OpenCV is a huge open-source library for computer vision, machine learning, and image processing. OpenCV supports a wide variety of programming languages like Python, C++, Java, etc. It can process images and videos to identify objects, faces, or even the handwriting of a human.
+import PIL.Image, PIL.ImageTk # pip install pillow    //python imaging library , Image Tk is used for showing image in Tkinter
 from functools import partial
-import threading
+#  Functools module is for higher-order functions that work on other functions. It provides functions for working with other functions and callable objects to use or extend them without completely rewriting them. This module has two classes – partial and partialmethod.
+#Partial class
+#A partial function is an original function for particular argument values. They can be created in Python by using “partial” from the functools library. The __name__ and __doc__ attributes are to be created by the programmer as they are not created automatically. Objects created by partial() have three read-only attributes:
+
+import threading    # we are using this for preventing the program from blocking , and take care of changing the responsibility of images and if mainloop() does not work our GUI will be hanged
 import time
 import imutils # pip install imutils
 
@@ -74,21 +80,23 @@ def not_out():
     print("Player is not out")
 
 # Width and height of our main screen
-SET_WIDTH = 650
+SET_WIDTH = 613
 SET_HEIGHT = 368
+# SET_WIDTH = 1788
+# SET_HEIGHT = 1073
 
 # Tkinter gui starts here
 window = tkinter.Tk()
 window.title("Shashwat Tyagi Third Umpire Decision Review Kit")
-cv_img = cv2.cvtColor(cv2.imread("welcome.png"), cv2.COLOR_BGR2RGB)
+cv_img = cv2.cvtColor(cv2.imread("lordsWelcome.jpg"), cv2.COLOR_BGR2RGB)
 canvas = tkinter.Canvas(window, width=SET_WIDTH, height=SET_HEIGHT)
 photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
-image_on_canvas = canvas.create_image(0, 0, ancho=tkinter.NW, image=photo)
-canvas.pack()
+image_on_canvas = canvas.create_image(0, 0, ancho=tkinter.NW, image=photo)   # NW-north-west, positioning
+canvas.pack() 
 
 
 # Buttons to control playback
-btn = tkinter.Button(window, text="<< Previous (fast)", width=50, command=partial(play, -25))
+btn = tkinter.Button(window, text="<< Previous (fast)", width=50, command=partial(play, -25)) # we can't give arguments in the function so we use partial so that ot behaves that we didn't pass the arguments
 btn.pack()
 
 btn = tkinter.Button(window, text="<< Previous (slow)", width=50, command=partial(play, -2))
@@ -105,4 +113,4 @@ btn.pack()
 
 btn = tkinter.Button(window, text="Give Not Out", width=50, command=not_out)
 btn.pack()
-window.mainloop()
+window.mainloop()                       # for running windows
